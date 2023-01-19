0f85800b5ca9026677d5175de369231b8bff363b
fatcamdog <camsamek@gmail.com>
2023-01-19 15:36:57 -0500

    Calculate points won for user and opponent

--- client/src/components/analytics/BasicAnalytics.tsx
+++ client/src/components/analytics/BasicAnalytics.tsx
@@ -3,7 +3,38 @@ import { FC } from 'react';
 import { IMatchUserProps } from '../../utils/interfaces';
 
 const BasicAnalytics: FC<IMatchUserProps> = ({ match, user }) => {
-  return <div>Basic Analytics</div>;
+  // calculate points won for user and opponent
+  const pointsWon = (user: boolean): number => {
+    let pointsWon = 0;
+
+    match.pointDetails.map((point) => {
+      if (point.won === user) pointsWon++;
+    });
+
+    return pointsWon;
+  };
+
+  return (
+    <div>
+      <div>
+        <p>{user.name}:</p>
+        <div>
+          <p>Points won: {pointsWon(true)}</p>
+          <p>Games won:</p>
+          <p>Sets won:</p>
+        </div>
+      </div>
+      <br />
+      <div>
+        <p>{match.opponent}:</p>
+        <div>
+          <p>Points won: {pointsWon(false)}</p>
+          <p>Games won:</p>
+          <p>Sets won:</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BasicAnalytics;

76a84596392c79413b809fd961168a64d686b790
fatcamdog <camsamek@gmail.com>
2023-01-30 22:07:34 -0500

    Opponents rally stats created

--- client/src/components/analytics/RallyingStats.tsx
+++ client/src/components/analytics/RallyingStats.tsx
@@ -133,12 +133,99 @@ const RallyingStats: FC = () => {
         </div>
       </div>
       <br />
-      {/* <div>
+      <div>
         <p>{match.opponent}:</p>
         <div>
-          <p>Add stuff here later</p>
+          <p>Unforced errors: {totalStats('opponent', 'unforced_error')}</p>
+          <p>Forced errors: {totalStats('opponent', 'forced_error')}</p>
+          <p>Forceing shots: {totalStats('opponent', 'forced_error')}</p>
+          <p>Winners: {totalStats('opponent', 'winner')}</p>
+          <p>
+            Unforced errors on forehand:{' '}
+            {typeSpecificStats('opponent', 'forehand', 'unforced_error')}
+          </p>
+          <p>
+            Unforced errors on forehand:{' '}
+            {typeSpecificStats('opponent', 'backhand', 'unforced_error')}
+          </p>
+          <p>
+            Unforced errors on overhead:{' '}
+            {typeSpecificStats('opponent', 'overhead', 'unforced_error')}
+          </p>
+          <p>
+            Forced errors on forehand:{' '}
+            {typeSpecificStats('opponent', 'forehand', 'forced_error')}
+          </p>
+          <p>
+            Forced errors on backhand:{' '}
+            {typeSpecificStats('opponent', 'backhand', 'forced_error')}
+          </p>
+          <p>
+            Forced errors on overhead:{' '}
+            {typeSpecificStats('opponent', 'overhead', 'forced_error')}
+          </p>
+          <p>
+            Winners on forehand:{' '}
+            {typeSpecificStats('opponent', 'forehand', 'winner')}
+          </p>
+          <p>
+            Winners on backhand:{' '}
+            {typeSpecificStats('opponent', 'backhand', 'winner')}
+          </p>
+          <p>
+            Winners on overhead:{' '}
+            {typeSpecificStats('opponent', 'overhead', 'winner')}
+          </p>
+          <p>
+            Unforced errors on groundstrokes:{' '}
+            {typeSpecificStats('opponent', 'groundstroke', 'unforced_error')}
+          </p>
+          <p>
+            Forced errors on groundstrokes:{' '}
+            {typeSpecificStats('opponent', 'groundstroke', 'forced_error')}
+          </p>
+          <p>
+            Winners on groundstrokes:{' '}
+            {typeSpecificStats('opponent', 'groundstroke', 'winner')}
+          </p>
+          <p>
+            Unforced errors on volleys:{' '}
+            {typeSpecificStats('opponent', 'volley', 'unforced_error')}
+          </p>
+          <p>
+            Forced errors on volleys:{' '}
+            {typeSpecificStats('opponent', 'volley', 'forced_error')}
+          </p>
+          <p>
+            Winners on volleys:{' '}
+            {typeSpecificStats('opponent', 'volley', 'winner')}
+          </p>
+          <p>
+            Unforced errors on slices:{' '}
+            {typeSpecificStats('opponent', 'slice', 'unforced_error')}
+          </p>
+          <p>
+            Forced errors on slices:{' '}
+            {typeSpecificStats('opponent', 'slice', 'forced_error')}
+          </p>
+          <p>
+            Winners on slices:{' '}
+            {typeSpecificStats('opponent', 'slice', 'winner')}
+          </p>
+          <p>
+            Unforced errors on dropshots:{' '}
+            {typeSpecificStats('opponent', 'dropshot', 'unforced_error')}
+          </p>
+          <p>
+            Forced errors on dropshots:{' '}
+            {typeSpecificStats('opponent', 'dropshot', 'forced_error')}
+          </p>
+          <p>
+            Winners on dropshots:{' '}
+            {typeSpecificStats('opponent', 'dropshot', 'winner')}
+          </p>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };

04a1dbbe16ecc335ab20719b8cd4e0a624be5f0d
fatcamdog <camsamek@gmail.com>
2023-02-08 11:59:12 -0500

    Conditionally rendering mental tracker stages

--- client/src/components/track/TrackMentality.tsx
+++ client/src/components/track/TrackMentality.tsx
@@ -5,10 +5,18 @@ import { useAppSelector } from '../../hooks/reduxHooks';
 import Timer from './Timer';
 import PointWonStage from '../stages/mentality/PointWonStage';
 import MentalReactionStage from '../stages/mentality/MentalReactionStage';
-import PointNotesStage from '../stages/mentality/PointsNotesStage';
+import PointNotesStage from '../stages/mentality/PointNotesStage';
 
 const TrackMentality: FC = () => {
+  // match duration counter
   const [duration, setDuration] = useState<number>(0);
+
+  // point stages variables
+  const [wonStage, setWonStage] = useState<boolean>(true);
+  const [userReactionStage, setUserReactionStage] = useState<boolean>(false);
+  const [opponentReactionStage, setOpponentReactionStage] =
+    useState<boolean>(false);
+  const [notesStage, setNotesStage] = useState<boolean>(false);
 
   const { match } = useAppSelector((state) => state.matches);
   const { user } = useAppSelector((state) => state.auth);
@@ -16,9 +24,29 @@ const TrackMentality: FC = () => {
   return (
     <div>
       <Timer match={match} duration={duration} setDuration={setDuration} />
-      <PointWonStage />
-      <MentalReactionStage />
-      <PointNotesStage />
+      {wonStage ? (
+        <PointWonStage />
+      ) : (
+        <>
+          {userReactionStage ? (
+            <MentalReactionStage />
+          ) : (
+            <>
+              {opponentReactionStage ? (
+                <MentalReactionStage />
+              ) : (
+                <>
+                  {notesStage ? (
+                    <PointNotesStage />
+                  ) : (
+                    <div>Something went wrong</div>
+                  )}
+                </>
+              )}
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 };

af4474c8b90b27f60374829e7e1e2ee9ca23e744
fatcamdog <camsamek@gmail.com>
2023-02-08 11:44:46 -0500

    Getting match data in mental tracker

--- client/src/components/track/TrackMentality.tsx
+++ client/src/components/track/TrackMentality.tsx
@@ -1,4 +1,16 @@
-const TrackMentality = () => {
+import { FC, useState } from 'react';
+
+import { IMatchUserProps } from '../../utils/interfaces';
+import { useAppSelector } from '../../hooks/reduxHooks';
+
+import Timer from './Timer';
+
+const TrackMentality: FC = () => {
+  const [duration, setDuration] = useState<number>(0);
+
+  const { match } = useAppSelector((state) => state.matches);
+  console.log(match);
+
   return <div>Tracking mentality</div>;
 };
 

5df23ca7d88b963aa6976a6189a216c786890bda
fatcamdog <camsamek@gmail.com>
2023-02-08 18:43:05 -0500

    handleMentalReaction function added

--- client/src/components/stages/mentality/MentalReactionStage.tsx
+++ client/src/components/stages/mentality/MentalReactionStage.tsx
@@ -14,13 +14,48 @@ const MentalReactionStage: FC<IHandleUserReactionProps> = ({
         What was the reaction of {user ? userName : opponentName} to the point?
       </p>
       <div>
-        <button className="btn btn-accent">They had no reaction</button>
-        <button className="btn btn-accent">They shouted positively</button>
-        <button className="btn btn-accent">They shouted negatively</button>
-        <button className="btn btn-accent">They fist-pumped</button>
-        <button className="btn btn-accent">They threw their racket</button>
-        <button className="btn btn-accent">They hit their racket</button>
-        <button className="btn btn-accent">Other</button>
+        <button
+          className="btn btn-accent"
+          onClick={() => handleMentalReaction(user, 'none')}
+        >
+          They had no reaction
+        </button>
+        <button
+          className="btn btn-accent"
+          onClick={() => handleMentalReaction(user, 'shout_positive')}
+        >
+          They shouted positively
+        </button>
+        <button
+          className="btn btn-accent"
+          onClick={() => handleMentalReaction(user, 'shout_negative')}
+        >
+          They shouted negatively
+        </button>
+        <button
+          className="btn btn-accent"
+          onClick={() => handleMentalReaction(user, 'fist_pump')}
+        >
+          They fist-pumped
+        </button>
+        <button
+          className="btn btn-accent"
+          onClick={() => handleMentalReaction(user, 'throw')}
+        >
+          They threw their racket
+        </button>
+        <button
+          className="btn btn-accent"
+          onClick={() => handleMentalReaction(user, 'hit')}
+        >
+          They hit their racket
+        </button>
+        <button
+          className="btn btn-accent"
+          onClick={() => handleMentalReaction(user, 'other')}
+        >
+          Other
+        </button>
       </div>
     </div>
   );

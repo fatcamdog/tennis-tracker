f134a978d49a22ef0a141de26ab487bc1434be20
fatcamdog <camsamek@gmail.com>
2023-02-11 14:57:14 -0500

    Change court style

--- client/src/components/court/ExactCourt.tsx
+++ client/src/components/court/ExactCourt.tsx
@@ -1,5 +1,16 @@
 const ExactCourt = () => {
-  return <div>Exact court</div>;
+  return (
+    <div>
+      <div className="relative after:content-[''] after:px-px after:py-2 after:absolute after:top-0 after:left-1/2 after:bg-white grid grid-areas-court grid-cols-court grid-rows-court w-fit outline outline-2 outline-white cursor-crosshair">
+        <div className="relative bg-blue-400 outline outline-2 outline-white grid-in-left-alley"></div>
+        <div className="flex gap-2 p-2 pr-0 bg-blue-400 grid-in-left-behind"></div>
+        <div className="flex justify-end gap-2 p-2 pl-0 bg-blue-400 grid-in-right-behind"></div>
+        <div className="flex gap-2 p-2 pr-0 bg-blue-400 outline outline-2 outline-white grid-in-deuce"></div>
+        <div className="flex justify-end gap-2 p-2 pl-0 bg-blue-400 outline outline-2 outline-white grid-in-ad"></div>
+        <div className="relative bg-blue-400 outline outline-2 outline-white grid-in-right-alley"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ExactCourt;

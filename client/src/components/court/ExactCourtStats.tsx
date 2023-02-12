3b9cf19f460ed07143b22bfcb340ab69ea494344
fatcamdog <camsamek@gmail.com>
2023-02-11 19:16:54 -0500

    Court diagram created

--- client/src/components/court/ExactCourtStats.tsx
+++ client/src/components/court/ExactCourtStats.tsx
@@ -1,3 +1,14 @@
 export const ExactDeuceStats = () => {
-  return <div>Exact deuce stats</div>;
+  return (
+    <div className="bg-green-500 p-32 flex justify-center">
+      <div className="relative after:content-[''] after:px-px after:py-2 after:absolute after:top-0 after:left-1/2 after:bg-white grid grid-areas-court grid-cols-court grid-rows-court w-fit outline outline-2 outline-white">
+        <div className="relative bg-blue-400 outline outline-2 outline-white grid-in-left-alley"></div>
+        <div className="flex gap-2 p-2 pr-0 bg-blue-400 grid-in-left-behind"></div>
+        <div className="flex justify-end gap-2 p-2 pl-0 bg-blue-400 grid-in-right-behind"></div>
+        <div className="flex gap-2 p-2 pr-0 bg-blue-400 outline outline-2 outline-white grid-in-deuce"></div>
+        <div className="flex justify-end gap-2 p-2 pl-0 bg-blue-400 outline outline-2 outline-white grid-in-ad"></div>
+        <div className="relative bg-blue-400 outline outline-2 outline-white grid-in-right-alley"></div>
+      </div>
+    </div>
+  );
 };

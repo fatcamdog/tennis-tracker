4916a6f1bdf40dccab0b046bcc8ce4f50cacfff8
fatcamdog <camsamek@gmail.com>
2023-02-12 12:08:19 -0500

    Green background created

--- client/src/components/court/ExactCourtStats.tsx
+++ client/src/components/court/ExactCourtStats.tsx
@@ -90,13 +90,15 @@ export const ExactServeStats: FC<IUserServingSideProps> = ({ user, side }) => {
 
 export const ExactRallyStats = () => {
   return (
-    <div className="relative after:content-[''] after:px-px after:py-2 after:absolute after:top-0 after:left-1/2 after:bg-white grid grid-areas-court grid-cols-court grid-rows-court w-fit outline outline-2 outline-white">
-      <div className="relative bg-blue-400 outline outline-2 outline-white grid-in-left-alley"></div>
-      <div className="flex gap-2 p-2 pr-0 bg-blue-400 grid-in-left-behind"></div>
-      <div className="flex justify-end gap-2 p-2 pl-0 bg-blue-400 grid-in-right-behind"></div>
-      <div className="flex gap-2 p-2 pr-0 bg-blue-400 outline outline-2 outline-white grid-in-deuce"></div>
-      <div className="flex justify-end gap-2 p-2 pl-0 bg-blue-400 outline outline-2 outline-white grid-in-ad"></div>
-      <div className="relative bg-blue-400 outline outline-2 outline-white grid-in-right-alley"></div>
+    <div className="bg-green-500 p-32 flex justify-center relative">
+      <div className="relative after:content-[''] after:px-px after:py-2 after:absolute after:top-0 after:left-1/2 after:bg-white grid grid-areas-court grid-cols-court grid-rows-court w-fit outline outline-2 outline-white">
+        <div className="relative bg-blue-400 outline outline-2 outline-white grid-in-left-alley"></div>
+        <div className="flex gap-2 p-2 pr-0 bg-blue-400 grid-in-left-behind"></div>
+        <div className="flex justify-end gap-2 p-2 pl-0 bg-blue-400 grid-in-right-behind"></div>
+        <div className="flex gap-2 p-2 pr-0 bg-blue-400 outline outline-2 outline-white grid-in-deuce"></div>
+        <div className="flex justify-end gap-2 p-2 pl-0 bg-blue-400 outline outline-2 outline-white grid-in-ad"></div>
+        <div className="relative bg-blue-400 outline outline-2 outline-white grid-in-right-alley"></div>
+      </div>
     </div>
   );
 };
